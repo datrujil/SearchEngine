c689cef3c75b9605fb5fb1eb55b3c1d9b68710c2
datrujil <91101091+datrujil@users.noreply.github.com>
2024-11-13 12:30:22 -0800

    Implemented JSON file dumping and dictionary conversion

--- Postings.py
+++ Postings.py
@@ -1,3 +1,5 @@
+import json
+
 class Postings:
     def __init__(self):
         self._doc_id = None
@@ -14,7 +16,15 @@ class Postings:
             self._frequency_postings[doc_id] = 1
 
     def write_frequency_posting_to_file(self, file):
-        for posting in self._frequency_postings.items():
-            file.write(f"{posting}\n")
-        file.write("-------------------------------------------------------\n\n")
+        json.dump(self._frequency_postings, file, ensure_ascii=False, indent=4)
+        # for posting in self._frequency_postings.items():
+        #     file.write(f"{posting}\n")
+        file.write("\n")
+
+    # Converts the posting data to a dictionary format for JSON export
+    def to_dict(self):
+        return {
+            "frequency_postings": self._frequency_postings
+            # Add other posting dictionaries here if needed in the future
+        }
 #################################################################### FREQUENCY POSTING METHODS ONLY
